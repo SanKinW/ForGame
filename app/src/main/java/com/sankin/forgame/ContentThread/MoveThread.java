846b674fc61ed78d80b3676c98d87e449cdf5fa4
SanKinW <18373275@buaa.edu.cn>
2021-02-14 20:02:30 +0800

    move init

--- app/src/main/java/com/sankin/forgame/ContentThread/MoveThread.java
+++ app/src/main/java/com/sankin/forgame/ContentThread/MoveThread.java
@@ -24,10 +24,17 @@ public class MoveThread extends Thread{
     @Override
     public void run() {
         while (flag) {
-            //随机移动
-            Random random = new Random();
-            int a = random.nextInt() % 3;
-            //做移动操作
+            synchronized (imageView) {
+                //随机移动
+                Random random = new Random();
+                int dis = random.nextInt() % 3 - 1;
+                int dir = random.nextInt() % 3;
+                //做移动操作
+                if (dir == 0) imageView.setX(imageView.getX() + dis);
+                else if (dir == 1) imageView.setY(imageView.getY() + dis);
+                else imageView.setZ(imageView.getZ() + dis);
+
+            }
             try {
                 sleep(1000);
             } catch (InterruptedException e) {
@@ -35,4 +42,5 @@ public class MoveThread extends Thread{
             }
         }
     }
+
 }
--- app/src/main/java/com/sankin/forgame/GameActivity.java
+++ app/src/main/java/com/sankin/forgame/GameActivity.java
@@ -1,29 +1,27 @@
 package com.sankin.forgame;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.view.View;
 import android.widget.ImageView;
 
 import com.sankin.forgame.ContentThread.MoveThread;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity implements SensorEventListener {
-    private float[] angel = new float[3];
-    private SensorManager sensorManager = null;
-    private Sensor gyroSensor = null;
+    private float[] angel = new float[3];  //陀螺仪数据
+    private SensorManager sensorManager = null; //传感器管理
+    private Sensor gyroSensor = null;   //陀螺仪
     private List<MoveThread> list = new ArrayList<>(); //敌人序列
-    private final static int MAX_ATTACKER = 10;
+    private final static int MAX_ATTACKER = 10; //最大敌人数量
+    private final static double STANDARD = Math.acos(Math.PI/18.0);
 
 
     @SuppressWarnings("deprecation")
@@ -31,17 +29,6 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        //添加敌人
-        /*
-        image = new ImageView(GameActivity.this);
-        image.setId(View.generateViewId());
-        image.setImageResource(R.drawable.ic_attacker);
-        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(R.dimen.enemy, R.dimen.enemy);
-        layoutParams.width = getResources().getDimensionPixelOffset(R.dimen.enemy);
-        layoutParams.height = getResources().getDimensionPixelOffset(R.dimen.enemy);
-        layoutParams.setMargins(400, 100, 0, 0);
-        addContentView(image, layoutParams);
-        */
 
         //获取传感器信息
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
@@ -83,23 +70,52 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-            angel[0] = event.values[0]; //x
-            angel[1] = event.values[1]; //y
-            angel[2] = event.values[2]; //z
             synchronized (list) {
+                angel[0] = event.values[0]; //x
+                angel[1] = event.values[1]; //y
+                angel[2] = event.values[2]; //z
                 checkDirection();
             }
         }
     }
 
+    /**
+     * 检查夹角
+     */
     public void checkDirection() {
         for (MoveThread move : list) {
             ImageView image = move.getImageView();
-            /*
-                检查角度，夹角小于10°认为瞄准，杀死该敌人
+            //检查角度，夹角小于10°认为瞄准，杀死该敌人
+            if (CalculationAngle(image)) {
                 move.setFlag(false);
                 list.remove(move);
-             */
+            }
+        }
+    }
+
+    /*
+        计算夹角
+     */
+
+    public boolean CalculationAngle(ImageView imageView) {
+        synchronized (imageView) {
+            float[] point = new float[3];
+            point[0] = imageView.getX();
+            point[1] = imageView.getY();
+            point[2] = imageView.getZ();
+            double dis1 = Math.sqrt(point[0] * point[0] + point[1] * point[1] + point[2] * point[2]);
+            double dis2 = Math.sqrt(angel[0] * angel[0] + angel[1] * angel[1] + angel[2] * angel[2]);
+            double multi = point[0] * angel[0] + point[1] * angel[1] + point[2] * angel[2];
+            double includedAngle = Math.acos(multi / (dis1 * dis2));
+            if (Double.compare(includedAngle, STANDARD) < 0) return true;
+            else {
+                //在一定范围内音效变化
+
+
+
+
+                return false;
+            }
         }
     }
 
@@ -120,7 +136,14 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
             }
             //随机生成敌人位置
             ImageView attacker = new ImageView(this);
-
+            int[] point = new int[3];
+            for (int i = 0; i < 3; ++i) {
+                point[i] = new Random().nextInt();
+                point[i] %= 6;
+            }
+            attacker.setX(point[0]);
+            attacker.setY(point[1]);
+            attacker.setZ(point[2]);
             MoveThread move = new MoveThread(attacker);
             move.start();
             list.add(move);
